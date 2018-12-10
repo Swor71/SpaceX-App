b3eca6b84914c2f066100534a3470d50c0df80bb
Marcin Swornowski <swor71@gmail.com>
2018-12-10 18:23:42 +0100

    Add CRA, gql query from the client, Launches and Launch component

---
+++ client/src/components/Launches.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import LaunchItem from './LaunchItem';
+
+const LAUNCHES_QUERY = gql`
+	query LAUNCHES_QUERY {
+		launches {
+			flight_number
+			mission_name
+			launch_date_local
+			launch_success
+		}
+	}
+`;
+
+export class Launches extends Component {
+	render() {
+		return (
+			<>
+				<h1 className="display-4 my-3">Launches</h1>
+				<Query query={LAUNCHES_QUERY}>
+					{({ loading, error, data }) => {
+						if (loading) return <h4>Loading...</h4>;
+						if (error) console.log(error);
+						console.log(data);
+
+						return (
+							<>
+								{data.launches.map((launch, index) => (
+									<LaunchItem key={index} launch={launch} />
+								))}
+							</>
+						);
+					}}
+				</Query>
+			</>
+		);
+	}
+}
+
+export default Launches;
